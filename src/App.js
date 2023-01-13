775ce46e29165ef2a372afbad0f05329fd93d5fc
Ok7150 <onkarvkale@official.com>
2023-01-13 18:09:33 +0530

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import Car from './components/Car'
+import Card from './components/Card'
+import Cards from './components/Cards'
+import Footer from './components/Footer'
+import Form from './components/Form'
+import Hero from './components/Hero'
+import NavBar from './components/NavBar'
+
 function App() {
   return (
     <div className="App">
-      
+      <NavBar />
     </div>
   );
 }
